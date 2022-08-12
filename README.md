66f24bcb8414ccdcfabd8bfd3aeb3e6a03bb2c89
Marcus Vinicius <marcusvdev@gmail.com>
2022-08-12 12:36:14 -0300

    Starting the game with score

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# game-mario
-Game Mario JS
+# Game Mario
+
+Developed with:
+
+<p>
+    <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+    <img src="https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white">
+    <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+    <img src="">
+    <img src="">
+</p>
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp          = require('gulp')
+const sass          = require('gulp-sass')(require('sass'))
+const sassGlob      = require('gulp-sass-glob')
+const autoprefixer  = require('gulp-autoprefixer')
+const gcmq          = require('gulp-group-css-media-queries')
+const cleanCSS      = require('gulp-clean-css')
+const minify        = require('gulp-minify')
+
+const css = () => gulp.src('./src/sass/*.scss')
+  .pipe(sassGlob())
+  .pipe(sass({ outputStyle: 'compressed' }))
+  .pipe(gcmq())
+  .pipe(autoprefixer({
+    overrideBrowserslist: ['last 3 versions'],
+    cascade: false
+  }))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('./dist/css/'))
+
+const js = () => gulp.src('./src/js/*.js')
+  .pipe(minify())
+  .pipe(gulp.dest('./dist/js/'))
+
+exports.serve = () => {
+  gulp.watch('./src/sass/**/*.scss', css).on('change', function(path, stats) {
+    console.log(`File ${path} was removed`);
+  });
+  gulp.watch('./src/js/*.js', js).on('change', function(path, stats) {
+    console.log(`File ${path} was removed`);
+  });
+}
